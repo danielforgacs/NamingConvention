3a9e47f51cb9604fca1034d38ec17c286e3ec09e
ford@AcerS1 <forgacs.daniel@gmail.com>
2018-10-25 22:10:29 +0100

    refactored; added int typed; added test; PASSED

--- basecls_test.py
+++ basecls_test.py
@@ -5,10 +5,20 @@ import pytest
 
 def test_source_is_string():
     name = basecls.BaseName()
+    name.source = '123'
 
     for typ in (1, (), [], 1.23):
         with pytest.raises(Exception):
             name.source = typ
+
+
+def test_count_is_int():
+    name = basecls.BaseName()
+    name.count = 1
+
+    for typ in ('1', (), [], 1.23):
+        with pytest.raises(Exception):
+            name.count = typ
 
 
 
