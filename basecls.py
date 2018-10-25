96adacdb1ef690e0975ff2dfd1da89e16fb4bb0e
ford@AcerS1 <forgacs.daniel@gmail.com>
2018-10-25 22:54:33 +0100

    fixed sized string; fixed exceptions; PASSED

--- basecls.py
+++ basecls.py
@@ -9,7 +9,7 @@ class Descriptor:
 class TypeBase(Descriptor):
     def __set__(self, instance, value):
         if not isinstance(value, self.typebase):
-            raise Exception('==> MUST BE TYPE: '+type(self.typbase))
+            raise Exception('==> MUST BE TYPE: '+str(self.typebase))
         super().__set__(instance, value)
 
 
@@ -19,15 +19,20 @@ class StringType(TypeBase):
 class IntType(TypeBase):
     typebase = int
 
-class SizedString(StringType):
+class SizedBase(Descriptor):
     def __init__(self, lenmin, lenmax):
         self.lenmin = lenmin
         self.lenmax = lenmax
 
     def __set__(self, instance, value):
         if not self.lenmin <= len(value) <= self.lenmax:
-            raise Exception('==> MUST BE LENGTH: ')
+            raise Exception('==> MUST BE LENGTH: %i - %i' % (self.lenmin, self.lenmax))
         super().__set__(instance, value)
+
+class SizedString(StringType, SizedBase):
+    pass
+
+
 
 
 # class TEMP:
