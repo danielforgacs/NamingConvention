8a2478c21de7a50da023d2eba45843f07c48f2d2
ford@AcerS1 <forgacs.daniel@gmail.com>
2018-11-07 23:05:53 +0000

    refactor; cleanup

--- basecls.py
+++ basecls.py
@@ -1,74 +1,81 @@
 class Descriptor(object):
     def __init__(self, attr):
         self.attr = attr
+
     def __set__(self, instance, value):
         instance.__dict__[self.attr] = value
+
     def __get__(self, instance, cls):
         if self.attr not in instance.__dict__.keys():
             return None
         return instance.__dict__[self.attr]
 
 
+
 class TypedAttr(Descriptor):
     def __set__(self, instance, value):
         if not isinstance(value, self.attrtype):
-            raise Exception('"%s" MUST BE TYPE %s' % (
-                self.attr, self.attrtype))
-        super(TypedAttr, self).__set__(instance, value)
+            value = None
+        super().__set__(instance, value)
+
 
 
 class StringTyped(TypedAttr):
     attrtype = str
 
+
+
 class IntTyped(TypedAttr):
     attrtype = int
+
+
 
 class LimitedIntBase(Descriptor):
     def __set__(self, instance, value):
         if not self.intmin <= value <= self.intmax:
-            # raise Exception('"%s" MUST BE MIN %s MAX %s' % (
-            #     self.attr, self.intmin, self.intmax))
             value = None
-        super(LimitedIntBase, self).__set__(instance, value)
+        super().__set__(instance, value)
+
 
 
 class LimitedInt(IntTyped, LimitedIntBase):
     def __init__(self, intmin, intmax, **kwargs):
-        super(LimitedInt, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.intmin, self.intmax = intmin, intmax
+
 
 
 class LenLimitBase(Descriptor):
     def __set__(self, instance, value):
         if not self.lenmin <= len(value) <= self.lenmax:
-            # raise Exception('"%s" MUST BE LENGTH %i - %i' % (
-            #     self.attr, self.lenmin, self.lenmax))
             value = None
-        super(LenLimitBase, self).__set__(instance, value)
+        super().__set__(instance, value)
+
 
 
 class LimitedString(StringTyped, LenLimitBase):
     def __init__(self, lmin, lmax, **kwargs):
-        super(LimitedString, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.lenmin, self.lenmax = lmin, lmax
+
 
 
 class OptionedAttr(Descriptor):
     def __init__(self, options, **kwargs):
-        super(OptionedAttr, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.options = options
 
     def __set__(self, instance, value):
         if value not in self.options:
-            # raise Exception('"%s" MUST BE: %s' % (self.attr, self.options))
             value = None
         super(OptionedAttr, self).__set__(instance, value)
+
 
 
 class NameMeta(type):
     def __new__(cls, name, bases, namespace):
         for attr, value in namespace['config'].items():
-            attrtype = value.keys()[0]
+            attrtype = tuple(value.keys())[0]
             values = value[attrtype]
             kwargs = {'attr': attr}
 
@@ -95,8 +102,8 @@ class NameMeta(type):
             else:
                 raise Exception('ATTR TYPE NOT IMPLEMENTED')
 
-        newclass = type(name, bases, namespace)
-        return newclass
+        return type(name, bases, namespace)
+
 
 
 class NameBase(object):
@@ -114,6 +121,8 @@ class NameBase(object):
 
 
 
+
+
 VARIANTCONF = {
     'integer': {'integer': None},
     'limitedinteger': {'limitedinteger': (100, 200)},
@@ -122,7 +131,7 @@ VARIANTCONF = {
     'region': {'options': ('aaa', 'bbb')},
 }
 
-class VariantName(NameBase):
+class VariantName(NameBase, metaclass=NameMeta):
     __metaclass__ = NameMeta
     config = VARIANTCONF
 
