e12d09924043874fe8478aae65b68dae0f049dc7
ford@AcerS1 <forgacs.daniel@gmail.com>
2018-11-07 22:39:55 +0000

    redesign

--- basecls.py
+++ basecls.py
@@ -1,182 +1,151 @@
-# module to create sanity checked strings like
-# product names. The string consist of multiple
-# elements. Rules for the elements come
-# from a configuration.
-
-
-class Descriptor:
-    """
-    Root descriptor class. All elements are set here
-    if they passed the tests in the subclasses.
-    The attributes name must be added.
-    """
+class Descriptor(object):
     def __init__(self, attr):
         self.attr = attr
-
+    def __set__(self, instance, value):
+        instance.__dict__[self.attr] = value
     def __get__(self, instance, cls):
-        if self.attr not in instance.__dict__:
+        if self.attr not in instance.__dict__.keys():
             return None
         return instance.__dict__[self.attr]
 
+
+class TypedAttr(Descriptor):
     def __set__(self, instance, value):
-        instance.__dict__[self.attr] = value
+        if not isinstance(value, self.attrtype):
+            raise Exception('"%s" MUST BE TYPE %s' % (
+                self.attr, self.attrtype))
+        super(TypedAttr, self).__set__(instance, value)
 
 
-class TypeBase(Descriptor):
-    """
-    This class checkes if the attribute is the
-    right type. Types are set in the sublcasses
-    "typebase" attribute.
-    """
-    def __set__(self, instance, value):
-        if not isinstance(value, self.typebase):
-            raise Exception('==> MUST BE TYPE: '+str(self.typebase))
-        super().__set__(instance, value)
+class StringTyped(TypedAttr):
+    attrtype = str
 
-
-class StringType(TypeBase):
-    typebase = str
-
-class IntType(TypeBase):
-    typebase = int
-
-
-class SizedStrBase(Descriptor):
-    """
-    Base class testing the lenght of a string.
-    Lenght is configured with the "lenmin"
-    and "lenmax" attributes.
-    """
-    def __init__(self, lenmin, lenmax, **kwargs):
-        super().__init__(**kwargs)
-        self.lenmin = lenmin
-        self.lenmax = lenmax
-
-    def __set__(self, instance, value):
-        if not self.lenmin <= len(value) <= self.lenmax:
-            raise Exception('==> MUST BE LENGTH: %i - %i' % (self.lenmin, self.lenmax))
-        super().__set__(instance, value)
-
-class SizedString(StringType, SizedStrBase):
-    pass
-
+class IntTyped(TypedAttr):
+    attrtype = int
 
 class LimitedIntBase(Descriptor):
-    """
-    Base class for testing integer limits.
-    Limits are configured with the "minint"
-    and "maxint" attrs.
-    """
-    def __init__(self, minint, maxint, **kwargs):
-        super().__init__(**kwargs)
-        self.minint = minint
-        self.maxint = maxint
-
     def __set__(self, instance, value):
-        if not self.minint <= value <= self.maxint:
-            raise Exception('==> MUST BE  %i < x < %i' % (self.minint, self.maxint))
-        super().__set__(instance, value)
+        if not self.intmin <= value <= self.intmax:
+            # raise Exception('"%s" MUST BE MIN %s MAX %s' % (
+            #     self.attr, self.intmin, self.intmax))
+            value = None
+        super(LimitedIntBase, self).__set__(instance, value)
 
 
-class LimitedInt(IntType, LimitedIntBase):
-    pass
+class LimitedInt(IntTyped, LimitedIntBase):
+    def __init__(self, intmin, intmax, **kwargs):
+        super(LimitedInt, self).__init__(**kwargs)
+        self.intmin, self.intmax = intmin, intmax
 
 
-class Optioned(Descriptor):
-    """
-    Main class for elements that have options.
-    Options are configured with the "options"
-    argument. "options" must be sequence type.
-    """
+class LenLimitBase(Descriptor):
+    def __set__(self, instance, value):
+        if not self.lenmin <= len(value) <= self.lenmax:
+            # raise Exception('"%s" MUST BE LENGTH %i - %i' % (
+            #     self.attr, self.lenmin, self.lenmax))
+            value = None
+        super(LenLimitBase, self).__set__(instance, value)
+
+
+class LimitedString(StringTyped, LenLimitBase):
+    def __init__(self, lmin, lmax, **kwargs):
+        super(LimitedString, self).__init__(**kwargs)
+        self.lenmin, self.lenmax = lmin, lmax
+
+
+class OptionedAttr(Descriptor):
     def __init__(self, options, **kwargs):
-        super().__init__(**kwargs)
+        super(OptionedAttr, self).__init__(**kwargs)
         self.options = options
 
     def __set__(self, instance, value):
-        if not value in self.options:
-            raise Exception('==> NOT AN OPTION')
-        super().__set__(instance, value)
+        if value not in self.options:
+            # raise Exception('"%s" MUST BE: %s' % (self.attr, self.options))
+            value = None
+        super(OptionedAttr, self).__set__(instance, value)
 
 
-
-class BaseNameMeta(type):
-    """
-    Metaclass to create the string classes.
-    It populates class dictionaries with elements
-    configured in the "conf" attr in the classes.
-    """
+class NameMeta(type):
     def __new__(cls, name, bases, namespace):
-        """
-        this is where string element classes get
-        set up based on the config.
-        """
-        for attr, attrtype in namespace['conf'].items():
-            if not isinstance(attrtype, dict):
-                namespace[attr] = None
-                continue
+        for attr, value in namespace['config'].items():
+            attrtype = value.keys()[0]
+            values = value[attrtype]
+            kwargs = {'attr': attr}
 
-            if tuple(attrtype.keys())[0] == 'choices':
-                choices = tuple(attrtype.values())[0]
-                namespace[attr] = Optioned(options=choices, attr=attr)
-            elif tuple(attrtype.keys())[0] == 'number':
-                namespace[attr] = IntType(attr=attr)
-            elif tuple(attrtype.keys())[0] == 'limitednumber':
-                minint, maxint = tuple(attrtype.values())[0]
-                namespace[attr] = LimitedInt(minint=minint, maxint=maxint, attr=attr)
+            if attrtype == 'text':
+                namespace[attr] = StringTyped(**kwargs)
 
-        newcls = type.__new__(cls, name, bases, namespace)
-        return newcls
+            elif attrtype == 'limitedtext':
+                kwargs['lmin'] = values[0]
+                kwargs['lmax'] = values[1]
+                namespace[attr] = LimitedString(**kwargs)
+
+            elif attrtype == 'integer':
+                namespace[attr] = IntTyped(**kwargs)
+
+            elif attrtype == 'limitedinteger':
+                kwargs['intmin'] = values[0]
+                kwargs['intmax'] = values[1]
+                namespace[attr] = LimitedInt(**kwargs)
+
+            elif attrtype == 'options':
+                kwargs['options'] = values
+                namespace[attr] = OptionedAttr(**kwargs)
+
+            else:
+                raise Exception('ATTR TYPE NOT IMPLEMENTED')
+
+        newclass = type(name, bases, namespace)
+        return newclass
 
 
-
-CONFIG = {
-    'a': {'choices': (1, 2)},
-    'b': {'choices': ('a', 'b')},
-    'c': {'number': None},
-    'd': {'limitednumber': (2, 5)},
-    'e': None,
-}
-
-class BaseName(metaclass=BaseNameMeta):
-    conf = CONFIG
-
+class NameBase(object):
     @property
     def name(self):
         if not self:
             return None
+        result = '_'.join([str(getattr(self, attr)) for attr in self.config.keys()])
+        return result
 
-        elements = (getattr(self, attr) for attr in self.conf.keys())
-        name = '_'.join(str(element) for element in elements)
-
-        return name
-
-    def __bool__(self):
-        allvalues = (getattr(self, attr) for attr in self.conf.keys())
-        allset = all(allvalues)
-        return allset
+    def __nonzero__(self):
+        result = all([getattr(self, attr) for attr in self.config.keys()])
+        return result
 
 
-name = BaseName()
-name.a = 1
-name.b = 'a'
-name.c = 1
-name.d = 4
-name.e = 'E'
 
-print(bool(name))
-print(name.name)
 
-# name.a = 0
-# name.b = 0
-# print(name.conf)
-# print(dir(name))
-# print(vars(name))
-# print(vars(BaseName))
-# print(BaseName.members)
-# print(name.members)
+VARIANTCONF = {
+    'integer': {'integer': None},
+    'limitedinteger': {'limitedinteger': (100, 200)},
+    'text': {'text': None},
+    'limitedtext': {'limitedtext': (1, 2)},
+    'region': {'options': ('aaa', 'bbb')},
+}
 
-# class TEMP:
-#     k = SizedString(lenmin=2, lenmax=3)
+class VariantName(NameBase):
+    __metaclass__ = NameMeta
+    config = VARIANTCONF
 
-# t = TEMP()
-# t.k = 1
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+    variant = VariantName()
+    variant.text = '1234'
+    variant.integer = 123
+    variant.limitedinteger = 200
+    variant.limitedtext = '12'
+    variant.region = 'aaa'
+
+    print(variant.text)
+    print(variant.integer)
+    print(variant.limitedinteger)
+    print(variant.limitedtext)
+    print(variant.region)
+    print()
+    print(bool(variant))
+    print(variant.name)
--- basecls_test.py
+++ basecls_test.py
@@ -1,89 +1,84 @@
-import basecls
 import pytest
+import basecls
 
 
 
-@pytest.mark.parametrize('value', (
-    1, (), [], 1.23,
+@pytest.mark.parametrize('type_, value', (
+    (int, 12345),
+    (str, 'skjdhf'),
+    (tuple, (1, 2, 3)),
+    (list, [1, 2, 3]),
+    (float, 1.234)
     ))
-def test_StringType_is_string_only(value):
-    class TestClass:
-        testattr = basecls.StringType(attr='testattr')
+def test_typed_class(type_, value):
+    class TestType(basecls.TypedAttr):
+        attrtype = type_
 
-    name = TestClass()
-    name.testattr = '123'
+    class TestName(object):
+        testattr = TestType(attr='testattr')
 
-    with pytest.raises(Exception):
-        name.testattr = value
-
-
-
-@pytest.mark.parametrize('value', (
-    '1', (), [], 1.23, ))
-def test_IntType_is_int_only(value):
-    class TestClass:
-        testattr = basecls.IntType(attr='testattr')
-
-    name = TestClass()
-    name.testattr = 123
-
-    with pytest.raises(Exception):
-        name.testattr = value
+    testname = TestName()
+    testname.testattr = value
 
 
 
-@pytest.mark.parametrize('value', (
-    '', 'a', 'aaaa', 1, ))
-def test_SizedString_has_limited_lenght(value):
-    class TestClass:
-        testattr = basecls.SizedString(attr='testattr', lenmin=2, lenmax=3)
+def test_LenLimited_class_01():
+    class LenLimited(basecls.LenLimitBase):
+        lenmin, lenmax = 2, 3
 
-    name = TestClass()
-    name.testattr = '12'
+    class TestName(object):
+        testattr = LenLimited(attr='testattr')
 
-    with pytest.raises(Exception):
-        name.testattr = value
+    testname = TestName()
+    testname.testattr = '123'
 
 
 
-@pytest.mark.parametrize('value', (
-    1, 'x', '23fks', ))
-def test_optioned(value):
-    class TestClass:
-        testattr = basecls.Optioned(attr='testattr', options=('a', 'b'))
+@pytest.mark.parametrize('lmin, lmax, value', (
+    (2, 3, '1'),
+    (2, 3, '1234'),
+    ))
+def test_LenLimited_class_02(lmin, lmax, value):
+    class LenLimited(basecls.LenLimitBase):
+        lenmin, lenmax = lmin, lmax
 
-    name = TestClass()
-    name.testattr = 'a'
+    class TestName(object):
+        testattr = LenLimited(attr='testattr')
 
-    assert name.testattr == 'a'
+    testname = TestName()
+    testname.testattr = value
 
-    name.testattr = 'b'
-
-    assert name.testattr == 'b'
-
-    with pytest.raises(Exception):
-        name.testattr = value
+    assert testname.testattr == None
 
 
 
 @pytest.mark.parametrize('value', (
-    -1, 0, 4, 5, 6, 7, ))
-def test_LimitedInt(value):
-    class TestClass:
-        testattr = basecls.LimitedInt(attr='testattr', minint=1, maxint=3)
+    2, 3, 'aaa'
+    ))
+def test_OptionedAttr_01(value):
+    class OptionedAttrTest(object):
+        testattr = basecls.OptionedAttr(attr='testattr', options=(2, 3, 'aaa'))
 
-    name = TestClass()
-    name.testattr = 1
-
-    assert name.testattr == 1
-
-    with pytest.raises(Exception):
-        name.testattr = value
+    testname = OptionedAttrTest()
+    testname.testattr = value
 
 
 
-def test_BaseName():
-    pass
+@pytest.mark.parametrize('value', (
+    1, 33, 'b'
+    ))
+def test_OptionedAttr_02(value):
+    class OptionedAttrTest(object):
+        testattr = basecls.OptionedAttr(attr='testattr', options=(2, 3, 'aaa'))
+
+    testname = OptionedAttrTest()
+    testname.testattr = value
+
+    assert testname.testattr == None
+
+
+
+
 
 
 
